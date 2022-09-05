44fc76a5e6668ef54733b53f8059bfe213b67149
Sergey_Korovkin <UPppZDPEDVdDrn4p6pfR>
2022-09-05 20:28:02 +0300

    My first commit

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import './Navigation.css';
+function Navigation() {
+	return (
+			<nav className={'nav'}>
+				<a href="https://www.google.com/">Сотрудники</a>
+				<a href="https://www.google.com/">Договоры</a>
+			</nav>
+	);
+}
+
+export default Navigation;
